b4338612cf65ef84fbad2ee8e0927452b173c40d
harshit <harshitpurohit@gkmit.co>
2024-10-29 18:55:00 +0530

    feat: update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './src/modules/users/users.module';
-import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from '.././orm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => typeOrmConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+//default
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { UsersModule } from './src/modules/users/users.module';
+// import { UsersModule } from './modules/users/users.module';
+
+// @Module({
+//   imports: [UsersModule],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
