5dc0a0c584ca5fdfdee12e58f44362dbb68fdec6
harshit <harshitpurohit@gkmit.co>
2024-10-29 18:58:04 +0530

    feat: install dotenv and configure .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from '.././orm';
+import { typeOrmConfig } from '.././ormconfig';
 
 @Module({
   imports: [
