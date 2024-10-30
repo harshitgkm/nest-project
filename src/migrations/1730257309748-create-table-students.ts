91f455131245c404738ed14cfca20dac95ae22c9
harshit <harshitpurohit@gkmit.co>
2024-10-30 08:35:18 +0530

    feat: create and run migrations

---
+++ src/migrations/1730257309748-create-table-students.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateUsersTable1730256185726 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        CREATE TABLE students
+        ("id" SERIAL NOT NULL,
+        "email" character varying NOT NULL,
+        "password" character varying NOT NULL,
+        "age" INT
+         );`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        DROP TABLE students;
+        `);
+  }
+}
